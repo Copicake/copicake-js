d471a93eacdbca49d0a30d72ebb7d85847aa6249
Jie Peng <im@jiepeng.me>
2022-06-19 15:16:10 +0800

    chore: moving MAX_RETRY_TIMES to constanst
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -3,3 +3,5 @@ export const API_END_POINT = "https://api.copicake.com/v1";
 export const IMAGE_API_END_POINT = `${API_END_POINT}/image`;
 
 export const RETRY_TIMEOUT = 1000;
+
+export const MAX_RETRY_TIMES = 10;
